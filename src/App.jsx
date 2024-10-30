9be9f85cf49a38bcd61531ba55a53bc92f56f99f
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2024-10-30 10:06:02 +0545

    HeroCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCard from './components/HomeCard'
+import JobListings from './components/JobListings'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
+      <HomeCard />
+      <JobListings />
     </>
   )
 }
