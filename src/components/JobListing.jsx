a0c8fa448a2f84fdbff0bec476efc33c3d33acc0
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2024-11-01 11:06:47 +0545

    Add Job Left

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { FaMapMarker } from "react-icons/fa";
 
 const JobListing = ({ job }) => {
@@ -31,12 +32,12 @@ const JobListing = ({ job }) => {
             <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
-          <a
-            href={`/job/${job.id}`}
+          <Link
+            to={`/job/${job.id}`}
             className="h-9 bg-indigo-500 hover:bg-indigo-600 text-white px-2 py-2 rounded-lg text-center text-sm"
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobsPage = () => {
+  return <div>Job Page here</div>;
+};
+
+export default JobsPage;
